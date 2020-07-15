e60b56821b9161681e558a97897408400bc10923
Nik Samoylov <nik@conjoint.ly>
2020-07-15 23:37:06 +1000

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,17 @@
-
 $(function() {
-    var $_GET = {};
-    if(document.location.toString().indexOf('?') !== -1) {
-        var query = document.location
-                       .toString()
-                       // get the query string
-                       .replace(/^.*?\?/, '')
-                       // and remove any existing hash string (thanks, @vrijdenker)
-                       .replace(/#.*$/, '')
-                       .split('&');
-
-        for(var i=0, l=query.length; i<l; i++) {
-           var aux = decodeURIComponent(query[i]).split('=');
-           $_GET[aux[0]] = aux[1];
-        }
+  var $_GET = {};
+  if (document.location.toString().indexOf('?') !== -1) {
+    var query = document.location
+      .toString()
+      .replace(/^.*?\?/, '')
+      .replace(/#.*$/, '')
+      .split('&');
+    for (var i = 0, l = query.length; i < l; i++) {
+      var aux = decodeURIComponent(query[i]).split('=');
+      $_GET[aux[0]] = aux[1];
     }
-  if (localStorage["pivotData"+$_GET["boardID"]] === undefined) {
+  }
+  if (localStorage["pivotData" + $_GET["boardID"]] === undefined) {
     var pivotData = {
       rows: ["List"],
       cols: ["Member"],
@@ -23,20 +19,13 @@ $(function() {
       rendererName: "Table"
     };
   } else {
-    var pivotData  = JSON.parse(localStorage["pivotData"+$_GET["boardID"]]);
+    var pivotData = JSON.parse(localStorage["pivotData" + $_GET["boardID"]]);
     delete pivotData.aggregators;
-    /*var pivotData = {
-      rows: pivotDataRaw.rows,
-      cols: pivotDataRaw.cols,
-      aggregatorName: pivotDataRaw.aggregatorName,
-      rendererName: pivotDataRaw.rendererName,
-      vals: pivotDataRaw.vals      
-    };*/
   }
   pivotData.onRefresh = function(config) {
-    localStorage.setItem("pivotData"+$_GET["boardID"], JSON.stringify(config));
+    localStorage.setItem("pivotData" + $_GET["boardID"], JSON.stringify(config));
     $(".pvtTable, table").css("width", "auto");
   };
-  $("#output").pivotUI(JSON.parse(localStorage['cardData'+$_GET["boardID"]]), pivotData);
+  $("#output").pivotUI(JSON.parse(localStorage['cardData' + $_GET["boardID"]]), pivotData);
   $(".pvtTable, table").css("width", "auto");
 });
