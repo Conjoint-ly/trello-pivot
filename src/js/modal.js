1c7c9217d7500404ff01842f930c4e07abdd9820
Nik Samoylov <nik@conjoint.ly>
2020-07-15 22:59:18 +1000

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,14 +7,14 @@ $(function() {
       rendererName: "Table"
     };
   } else {
-    var pivotDataRaw  = JSON.parse(localStorage.pivotData);
-    var pivotData = {
+    var pivotData  = JSON.parse(localStorage.pivotData);
+    /*var pivotData = {
       rows: pivotDataRaw.rows,
       cols: pivotDataRaw.cols,
       aggregatorName: pivotDataRaw.aggregatorName,
       rendererName: pivotDataRaw.rendererName,
       vals: pivotDataRaw.vals      
-    };
+    };*/
   }
   pivotData.onRefresh = function(config) {
     localStorage.setItem("pivotData", JSON.stringify(config));
