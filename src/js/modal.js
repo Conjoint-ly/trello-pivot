2acb747721020e6a19f8936078930be997265bff
Nik Samoylov <nik@conjoint.ly>
2020-07-15 22:10:43 +1000

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,19 @@
-$("#output").pivotUI(
- JSON.parse(localStorage.cardData), {
-    rows: ["List"],
-    cols: ["Member"],
-    aggregatorName: "Count",
-    rendererName: "Table"
-  });
+$(window).load(function() {
+  if (localStorage.pivotData === undefined) {
+    let pivotData = {
+      rows: ["List"],
+      cols: ["Member"],
+      aggregatorName: "Count",
+      rendererName: "Table"
+    };
+  } else {
+    let pivotData = JSON.parse(localStorage.cardData);
+  }
+  settings.onRefresh = function(config) {
+    localStorage.setItem("pivotData", JSON.stringify(config));
+    $(".pvtTable").css("width", "");
+  };
+
+  $("#output").pivotUI(JSON.parse(localStorage.cardData), pivotData);
+
+});
