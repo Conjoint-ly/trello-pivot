6957d8f465a0fe010299ade98669fd20386f0a3c
Nik Samoylov <nik@conjoint.ly>
2020-07-15 23:17:08 +1000

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,21 @@
+var $_GET = {};
+if(document.location.toString().indexOf('?') !== -1) {
+    var query = document.location
+                   .toString()
+                   // get the query string
+                   .replace(/^.*?\?/, '')
+                   // and remove any existing hash string (thanks, @vrijdenker)
+                   .replace(/#.*$/, '')
+                   .split('&');
+
+    for(var i=0, l=query.length; i<l; i++) {
+       var aux = decodeURIComponent(query[i]).split('=');
+       $_GET[aux[0]] = aux[1];
+    }
+}
+
 $(function() {
-  if (localStorage.pivotData === undefined) {
+  if (localStorage["pivotData"+$_GET["boardID"]] === undefined) {
     var pivotData = {
       rows: ["List"],
       cols: ["Member"],
@@ -7,7 +23,7 @@ $(function() {
       rendererName: "Table"
     };
   } else {
-    var pivotData  = JSON.parse(localStorage.pivotData);
+    var pivotData  = JSON.parse(localStorage["pivotData"+$_GET["boardID"]]);
     delete pivotData.aggregators;
     /*var pivotData = {
       rows: pivotDataRaw.rows,
@@ -18,9 +34,9 @@ $(function() {
     };*/
   }
   pivotData.onRefresh = function(config) {
-    localStorage.setItem("pivotData", JSON.stringify(config));
+    localStorage.setItem("pivotData"+$_GET["boardID"], JSON.stringify(config));
     $(".pvtTable, table").css("width", "auto");
   };
-  $("#output").pivotUI(JSON.parse(localStorage.cardData), pivotData);
+  $("#output").pivotUI(JSON.parse(localStorage['cardData'+$_GET["boardID"]]), pivotData);
   $(".pvtTable, table").css("width", "auto");
 });
